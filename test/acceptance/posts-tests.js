6911dbb89143f2795b061a0ce8f23fdfe1be1a69
Will Munn <will.munn@focusrite.com>
2022-08-17 22:20:23 +0100

    Refactor tests to use AssertingResponse class

--- test/acceptance/posts-tests.js
+++ test/acceptance/posts-tests.js
@@ -20,22 +20,34 @@ class TestRequest extends Readable {
   }
 }
 
+class AssertingResponse {
+  #actualStatus;
+  #expectedStatus;
+  #expectedBody;
+  #end;
+  constructor(expectedStatus, expectedBody, end) {
+    this.#expectedStatus = expectedStatus;
+    this.#expectedBody = expectedBody;
+    this.#end = end;
+  }
+
+  writeHead(status, _headers) {
+    this.#actualStatus = status;
+  }
+
+  end(body) {
+    assert.equal(this.#actualStatus, this.#expectedStatus);
+    assert.deepEqual(this.#expectedBody, JSON.parse(body));
+    this.#end();
+  }
+}
+
 test('Posts API', async t => {
   await t.test('Getting posts', async () => {
     return await new Promise((resolve) => {
       const app = App();
       const request = new TestRequest('GET', '/posts', null);
-      let actualStatus;
-      const response = {
-        writeHead(status, _headers) {
-          actualStatus = status;
-        },
-        end(body) {
-          assert.equal(actualStatus, 200);
-          assert.deepEqual({posts: []}, JSON.parse(body));
-          resolve();
-        }
-      };
+      const response = new AssertingResponse(200, {posts: []}, resolve);
       app(request, response);
     });
   });
@@ -53,19 +65,9 @@ test('Posts API', async t => {
         '/posts',
         JSON.stringify(post)
       );
-      let actualStatus;
-      const response = {
-        writeHead(status, _headers) {
-          actualStatus = status;
-        },
-        end(body) {
-          assert.equal(actualStatus, 201);
-          assert.deepEqual({
-            posts: [post.date.toISOString()]
-          }, JSON.parse(body));
-          resolve();
-        }
-      };
+      const response = new AssertingResponse(201, {
+        posts: [post.date.toISOString()]
+      }, resolve);
       app(request, response);
     });
   });
